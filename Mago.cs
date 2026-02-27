08fac0b0628646186eb40f0d34824985bb6e181d
alvaro <alvarotuning39@gmail.com>
2026-02-27 13:11:50 +0100

    documentacion de mago y Configuracion de generacion de documentacion xml [Alvaro Collado]

--- Mago.cs
+++ Mago.cs
@@ -7,31 +7,46 @@ namespace ProyectoRPG
     /// </summary>
     public class Mago : Personaje
     {
-        //public int mana;
+        /// <summary>
+        /// Este atributo se encarga del control del mana esta encapsulado
+        /// </summary>
         private int mana;
+        /// <summary>
+        /// Propiedad para obtener o establecer el mana
+        /// </summary>
         public int Mana
         {
             get => mana;
             set => mana = value;
         }
         
-
+        /// <summary>
+        /// Almacena el nombre del hechizo
+        /// </summary>
         public string hechizo;
-
+        /// <summary>
+        /// Constructor de mago que se inicializa con valores por defecto
+        /// </summary>
         public Mago()
         {
             
             this.Mana = 100;
             this.hechizo = "Bola de fuego";
         }
-
+        /// <summary>
+        /// Constructor que inicializa al mago con un nombre especifico llamndo al constructor base
+        /// </summary>
+        /// <param name="nombre"></param>
         public Mago(string nombre):base(nombre)
         {
             //this.nombre = nombre;
             this.Mana = 100;
             this.hechizo = "bola de fuego";
         }
-
+        
+        /// <summary>
+        /// Realiza el lanzamiento de un hechizo si se dispone de mana suficiente
+        /// </summary>
         public void LanzarHechizo()
         {
             if (Mana >= 20)
@@ -45,7 +60,9 @@ namespace ProyectoRPG
                 Console.WriteLine("Mana insuficiente para lanzar el hechizo.");
             }
         }
-
+        /// <summary>
+        /// Incrementa los puntos de mana del mago mediante meditacion, con limite de 100
+        /// </summary>
         public void Meditar()
         {
             Mana += 30;
