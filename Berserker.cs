82b6a9fbd015c700b33c2512dbaa8158a1974f98
Francisco Mulero <2906500@alu.murciaeduca.es>
2026-03-02 19:16:14 +0100

    Documentación de la clase berserker

--- Berserker.cs
+++ Berserker.cs
@@ -2,29 +2,40 @@ using System;
 
 namespace ProyectoRPG
 {
+    /// <summary>
+    ///Clase Berserker que hereda de la clase Guerrero
+    /// </summary>
     public class Berserker : Guerrero
     {
         public int furia;
-
+        /// <summary>
+        /// Constructor de la clase
+        /// </summary>
         public Berserker()
         {
             this.furia = 0;
             this.arma = "Hacha de batalla";
             this.fuerza = 20;
         }
-		
+        /// <summary>
+        /// Constructor de la clase
+        /// </summary>
+        /// <param name="nivel"></param>
 		public Berserker(int nivel)
 		{
 			this.nivel = nivel;
 		}
-		
-
+        /// <summary>
+        /// Cambio de estado del personaje
+        /// </summary>
         public void EntrarEnFuria()
         {
             furia += 30;
             Console.WriteLine(Nombre + " entra en estado de furia! Furia actual: " + furia);
         }
-
+        /// <summary>
+        /// Ataque furioso del Berserker
+        /// </summary>
         public void AtaqueFurioso()
         {
             if (furia >= 50)
@@ -38,10 +49,10 @@ namespace ProyectoRPG
                 Console.WriteLine("Furia insuficiente para realizar ataque furioso.");
             }
         }
-
-		
-
-
+        /// <summary>
+        /// Muestra información del personaje
+        /// </summary>
+        /// <returns> Nombre, Clase, Nivel, Vida, Fuerza, Arma, Furia </returns>
         public string MostrarInfo()
         {
             return "=== INFORMACIÓN DEL PERSONAJE ===" +
